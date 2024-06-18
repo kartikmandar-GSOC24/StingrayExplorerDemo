cf27ff27588ebaaf7390480072eb891b94459c5f
kartikmandar <kartik4321mandar@gmail.com>
2024-06-18 23:13:43 +0530

    git rm __pycache__

--- functions/LightCurve/DataIngestion.py
+++ functions/LightCurve/DataIngestion.py
@@ -11,42 +11,42 @@ def create_data_ingestion_tab():
     data_ingestion = None
     detector_added = False
 
-    # Function to handle detector information addition
-    def handle_add_detector(event):
-        nonlocal data_ingestion, detector_added
-        detector_ids = detector_input.value.split(",")
-        try:
-            detector_ids = [int(det_id.strip()) for det_id in detector_ids]
-            data_ingestion.add_detector_information(detector_ids)
-            confirmation_text.object = "Detector information added successfully."
-            detector_added = True  # Set flag to indicate detector info is added
-        except ValueError as ve:
-            confirmation_text.object = str(ve)
+    # # Function to handle detector information addition
+    # def handle_add_detector(event):
+    #     nonlocal data_ingestion, detector_added
+    #     detector_ids = detector_input.value.split(",")
+    #     try:
+    #         detector_ids = [int(det_id.strip()) for det_id in detector_ids]
+    #         data_ingestion.add_detector_information(detector_ids)
+    #         confirmation_text.object = "Detector information added successfully."
+    #         detector_added = True  # Set flag to indicate detector info is added
+    #     except ValueError as ve:
+    #         confirmation_text.object = str(ve)
 
     # Function to handle file upload and processing
     def handle_file_upload(event):
         nonlocal filename, data_ingestion, detector_added
-        if not detector_added:
-            confirmation_text.object = "Please add detector information first."
-            return
+        # if not detector_added:
+        #     confirmation_text.object = "Please add detector information first."
+        #     return
 
         file_path = event.new
         filename = os.path.basename(file_path)
 
-        # Check if the file path exists and is a file
-        if os.path.exists(file_path) and os.path.isfile(file_path):
-            # Initialize DataIngestion object
-            data_ingestion = DataIngestion(file_path)
+        # # Check if the file path exists and is a file
+        # if os.path.exists(file_path) and os.path.isfile(file_path):
+        #     # Initialize DataIngestion object
+        #     data_ingestion = DataIngestion(file_path)
 
-            # Load events from the file
-            try:
-                data_ingestion.load_events(additional_columns=["DET_ID"])
-                # Display confirmation message
-                confirmation_text.object = f"Successfully loaded events from {filename}"
-            except Exception as e:
-                confirmation_text.object = f"Error loading events from {filename}: {str(e)}"
-        else:
-            confirmation_text.object = f"File '{filename}' does not exist or is not a valid file."
+        #     # Load events from the file
+        #     try:
+        #         data_ingestion.load_events(additional_columns=["DET_ID"])
+        #         # Display confirmation message
+        #         confirmation_text.object = f"Successfully loaded events from {filename}"
+        #     except Exception as e:
+        #         confirmation_text.object = f"Error loading events from {filename}: {str(e)}"
+        # else:
+        #     confirmation_text.object = f"File '{filename}' does not exist or is not a valid file."
 
     # Function to display first few elements of the loaded file
     def display_first_elements(event):
