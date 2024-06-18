0e732fd77a0720b6f213c6f11600fbfb5ce6c864
kartikmandar <kartik4321mandar@gmail.com>
2024-06-18 23:35:11 +0530

    add strings to module

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import panel as pn
 from functions.LightCurve.LightCurveAnalysisPanel import create_light_curve_analysis_panel
+from utils.textual_strings import WELCOME_MESSAGE, HEADER_STRING
+
 
 # Load external stylesheet
 external_stylesheets = ['./assets/stylesheets/app.css']
@@ -9,28 +11,17 @@ pn.extension(notifications=True)
 
 # Create header
 header = pn.pane.Markdown(
-    "<h1 style='text-align: center'>Welcome to the Stingray Explorer Dashboard</h1>",
+    HEADER_STRING
 )
 
 # Create a welcome message
 welcome_message = pn.pane.Markdown(
-    """
-    <div style='text-align: center'>
-        <p>Welcome to the Stingray Explorer Dashboard! This dashboard is designed to provide a comprehensive toolset for X-ray astronomy data analysis. Here are the main features:</p>
-        <ul>
-            <li><b>Light Curve Analysis:</b> Visualize and analyze light curves to study the variability of X-ray sources over time.</li>
-            <li><b>Power Spectrum Analysis:</b> Generate power spectra to investigate the periodic signals and frequency components of your data.</li>
-            <li><b>Cross Spectrum Analysis:</b> Cross-correlate different light curves to explore phase lags and coherence between signals.</li>
-        </ul>
-        <p>Please use the sidebar to navigate to the different analysis tools. Each tool comes with interactive widgets to customize your analysis and generate plots on the fly.</p>
-        <p>We hope you find this dashboard useful for your research!</p>
-    </div>
-    """,
-    styles={"font-size": "16px"}
+    WELCOME_MESSAGE,
+    styles={"font-size": "16px", "align": "start"}
 )
 
 # Create sidebar with buttons for navigation
-light_curve_button = pn.widgets.Button(name="Light Curve", button_type="primary")
+light_curve_button = pn.widgets.Button(name="Light Curve")
 
 # Define button behaviors
 def show_light_curve(event):
@@ -50,24 +41,25 @@ main = pn.Row(
     pn.Column(
         header,
         welcome_message,
-        css_classes=['main-content']  # Apply CSS class to main content
+        css_classes=['main-content'],  # Apply CSS class to main content
+        align="center"
     ),
     sizing_mode="stretch_width"
 )
 
 # Create a template with a sidebar that can be toggled with a hamburger menu
-template = pn.template.MaterialTemplate(
+layout = pn.template.MaterialTemplate(
     title="Stingray Explorer",
     logo="./assets/images/stingray_logo.png",
     collapsed_sidebar=True,
     sidebar=[sidebar],
     main=[main],
     header_background="#000000",
-    sidebar_width=200,
+    sidebar_width=150,
 )
 
-# Serve the template
-template.servable()
+# Serve the layout
+layout.servable()
 
 if __name__ == "__main__":
-    pn.serve(template)
+    pn.serve(layout)
---
+++ utils/textual_strings.py
@@ -0,0 +1,17 @@
+## This section contains the textual strings used in the app.py
+
+HEADER_STRING = "<h1>Welcome to the Stingray Explorer Dashboard</h1>"
+
+WELCOME_MESSAGE = """
+<div style='text-align: center'>
+    <p>This dashboard is designed to provide a comprehensive toolset for X-ray astronomy data analysis. Here are the main features:</p>
+    <ul>
+        <li><b>Light Curve Analysis:</b> Visualize and analyze light curves to study the variability of X-ray sources over time.</li>
+        <li><b>Power Spectrum Analysis:</b> Generate power spectra to investigate the periodic signals and frequency components of your data.</li>
+        <li><b>Cross Spectrum Analysis:</b> Cross-correlate different light curves to explore phase lags and coherence between signals.</li>
+    </ul>
+    <p>Please use the sidebar to navigate to the different analysis tools. Each tool comes with interactive widgets to customize your analysis and generate plots on the fly.</p>
+    <p>We hope you find this dashboard useful for your research!</p>
+</div>
+"""
+
